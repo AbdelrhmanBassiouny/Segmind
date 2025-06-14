c1fc81b35f73b2764b8d99e01b7835acfc3da6e5
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-06-14 13:07:04 +0200

    [Segmind] update pycram branch in Dockerfile and update entrypoint and post_create_setup.sh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -14,7 +14,9 @@ pip install -r "$WORKSPACE_DIR/requirements.txt"
 pip install -e "$WORKSPACE_DIR"
 cd "$WORKSPACE_DIR/../ripple_down_rules" && git pull && pip install -r "requirements.txt" && pip install -e .
 cd "$WORKSPACE_DIR/../semantic_world" && git pull && pip install -r "requirements.txt" && pip install -e .
-cd "$WORKSPACE_DIR/../Multiverse-Parser" && git pull && pip install -r "requirements.txt" && pip install -e .
-cd "$WORKSPACE_DIR/../Multiverse-Resources" && git pull && git submodule update --init --recursive
+cd "$WORKSPACE_DIR/../pycram" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/giskardpy" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/Multiverse-Parser" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/Multiverse-Resources" && git pull && git submodule update --init --recursive
 
 exec "$@"
--- .devcontainer/post_create_setup.sh
+++ .devcontainer/post_create_setup.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # workspace directory
 WORKSPACE_DIR="/root/workspace/src/Segmind"
+LIBS_DIR="/root/libs"
 
 # Wait for files to be mounted
 while [ ! -f "$WORKSPACE_DIR/requirements.txt" ]; do
@@ -16,5 +17,7 @@ pip install -r "$WORKSPACE_DIR/requirements.txt"
 pip install -e "$WORKSPACE_DIR"
 cd "$WORKSPACE_DIR/../ripple_down_rules" && git pull && pip install -r "requirements.txt" && pip install -e .
 cd "$WORKSPACE_DIR/../semantic_world" && git pull && pip install -r "requirements.txt" && pip install -e .
-cd "$WORKSPACE_DIR/../Multiverse-Parser" && git pull && pip install -r "requirements.txt" && pip install -e .
-cd "$WORKSPACE_DIR/../Multiverse-Resources" && git pull && git submodule update --init --recursive
+cd "$WORKSPACE_DIR/../pycram" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/giskardpy" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/Multiverse-Parser" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$LIBS_DIR/Multiverse-Resources" && git pull && git submodule update --init --recursive
