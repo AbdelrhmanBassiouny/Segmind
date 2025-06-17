55f55337e3eb74c4911a0be5792e9d7f8b16849e
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-06-18 01:37:34 +0200

    [Segmind] docker updates.

--- .devcontainer/ros1_noetic/entrypoint.sh
+++ .devcontainer/ros1_noetic/entrypoint.sh
@@ -2,25 +2,27 @@
 
 set -e
 
-if [ "$ROS_DISTRO" = "noeitc" ]; then
+if [ "$ROS_DISTRO" = "noetic" ]; then
     source /opt/ros/noetic/setup.bash
 else
     source /opt/ros/jazzy/setup.bash
 fi
 
-source /root/workspace/install/setup.bash
+source /root/workspace/devel/setup.bash
 source /root/.virtualenvs/pycram-segmind/bin/activate
 
 # workspace directory
 WORKSPACE_DIR="/root/workspace/src/Segmind"
+LIBS_DIR="/root/libs"
 
 # Your setup commands
 pip install -r "$WORKSPACE_DIR/requirements.txt"
 pip install -e "$WORKSPACE_DIR"
 cd "$WORKSPACE_DIR/../ripple_down_rules" && git pull && pip install -r "requirements.txt" && pip install -e .
 cd "$WORKSPACE_DIR/../semantic_world" && git pull && pip install -r "requirements.txt" && pip install -e .
-cd "$WORKSPACE_DIR/../pycram" && git checkout icub_demo_ros2 && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$WORKSPACE_DIR/../pycram" && git checkout icub_demo && git pull && pip install -r "requirements.txt" && pip install -e .
 cd "$LIBS_DIR/giskardpy" && git pull && pip install -r "requirements.txt" && pip install -e .
+cd "$WORKSPACE_DIR/../giskardpy_ros" && git pull && pip install -r "requirements.txt" && pip install -e .
 cd "$LIBS_DIR/Multiverse" && git pull && git submodule update --init --recursive
 
 exec "$@"
